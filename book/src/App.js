275136502ef0ac0519bbdecc438e9a1a11cfd173
Mike Doerfler <mike.doerfler@casemax.com>
2023-06-08 18:36:41 -0500

    initial commit for ch 4

---
+++ book/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ book/src/Counter.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {DECREMENT, INCREMENT} from "./actions";
+import {connect} from "react-redux";
+
+function Counter(props) {
+    return (
+        <div>
+            <div>{props.counter}</div>
+            <button
+                type="button"
+                onClick={props.onIncrement}>
+                +
+            </button>
+            <button
+                type="button"
+                onClick={props.onDecrement}>
+                -
+            </button>
+        </div>
+
+    );
+}
+
+function mapStateToProps(state){
+    return {
+        counter: state
+    };
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+        onIncrement() {
+            dispatch({type: INCREMENT});
+        },
+        onDecrement() {
+            dispatch({type: DECREMENT});
+        }
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
